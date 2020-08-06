c73ac161fc974927523d08fa40e1ab2515e08bb1
Leila <nb.leila10@gmail.com>
2020-08-05 19:20:32 -0500

    Make other errors in the function call that will be caught by typescript later

--- index.ts
+++ index.ts
@@ -14,9 +14,13 @@ axios.get(url).then((response) => {
   const title = todo.title;
   const completed = todo.completed;
 
-  console.log(`
-        The todo with id: ${id}
-        Has a title of: ${title}
-        Is it finished? ${completed}
-    `);
+  logTodo(id, completed, title);
 });
+
+const logTodo = (id, title, completed) => {
+  console.log(`
+    The todo with id: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+`);
+};
